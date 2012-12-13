60884096756c206d12a9b0e1c0f47d074d1143f6
alumno <alumno@alumno-VirtualBox.(none)>
2012-12-12 21:06:29 -0300

    Added js code

--- drupal/sites/all/themes/custom/company/js/user_tweets.js
+++ drupal/sites/all/themes/custom/company/js/user_tweets.js
@@ -1,24 +1,35 @@
-;(function (global) {
-  
-  
+;(function (global) {  
   
   global.addEventListener("load", function () {
     console.log("Dom Loaded");
     getUserTimeline();    
-  });
+  });    
   
+  var updateTemplate = function(userTimeline) {    
+    var itemCont = 0;
+    var listContainer = '<div class="myTweeterList">';
+    listContainer = listContainer + '<ul>';
+    for(itemCont; itemCont < userTimeline.data.length; itemCont++) {
+      listContainer = listContainer + '<li>';
+      listContainer = listContainer + userTimeline.data[itemCont].text;
+      listContainer = listContainer + '</li>';
+    }
+    listContainer = listContainer + '</ul>';
+    listContainer = listContainer + '</div>';    
+    var htmlTarget = document.getElementById('footer-bottom-dash');
+    htmlTarget.innerHTML = listContainer;
+  }
   
   var getUserTimeline = function () {
     var self = this, 
-      xhr = new XMLHttpRequest();
-      
-    xhr.open("GET", 'news-ajax/twitter/get-timeline' ,true);
+      xhr = new XMLHttpRequest();      
+    xhr.open("GET", 'news-ajax/twitter/get-timeline');
     xhr.addEventListener("load", function (data) {
+
       var userTimeline = JSON.parse(data.target.response)
-      console.log(data);
-      console.log(data.target.response);
-      console.log(userTimeline);
+      updateTemplate(userTimeline);      
     });    
     xhr.send();
   }
+  
 })(this);
